cd93629159836e6c8414d2e2f78289b00342ba2e
Matthew Thompson <matthew.thompson@nasa.gov>
2023-08-24 13:03:22 -0400

    Fix GEOSpyD

--- Core/GEOSpyD/Min23.5.2-0_py3.11.lua
+++ Core/GEOSpyD/Min23.5.2-0_py3.11.lua
@@ -19,7 +19,7 @@ local installdir = pathJoin(homedir,"installed/Core/GEOSpyD")
 local miniconda_version = "23.5.2-0"
 local python_version = "3.11"
 local geospyd_version = miniconda_version .. "_py" .. python_version
-local builddate = "2023-07-17"
+local builddate = "2023-08-24"
 
 local pathdir = pathJoin(geospyd_version,builddate)
 local pkgdir = pathJoin(installdir,pathdir)
