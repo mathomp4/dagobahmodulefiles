e33adcb0a957bf2e7dbca5930820e3a782b5f8d9
Matthew Thompson <matthew.thompson@nasa.gov>
2024-10-18 08:56:16 -0400

    Clean up GEOSpyD

--- Core/GEOSpyD/24.7.1-2/3.12.lua
+++ Core/GEOSpyD/24.7.1-2/3.12.lua
@@ -6,7 +6,7 @@ Installed using the GEOSpyD installer script on github:
 
 and I ran
 
-  ./install_miniconda.bash --python_version 3.12 --miniconda_version 24.1.2-0 --prefix /Users/mathomp4/installed/Core/GEOSpyD |& tee install.24.1.2-0_py3.12.log
+  ./install_miniforge.bash --python_version 3.12 --miniforge_version 24.7.1-2 --prefix /Users/mathomp4/installed/Core/GEOSpyD |& tee install.24.7.1-2_py3.12.log
 
 NOTE: You'll need to set the date below!
 --]]
@@ -16,18 +16,17 @@ family("Python3")
 local homedir = os.getenv("HOME")
 local installdir = pathJoin(homedir,"installed/Core/GEOSpyD")
 
-local miniconda_version = "24.1.2-0"
-local python_version = "3.12"
-local geospyd_version = miniconda_version .. "_py" .. python_version
-local builddate = "2024-03-25"
+local miniforge_version = "24.7.1-2"
+local python_version = "py3.12"
+local builddate = "2024-10-18"
 
-local pathdir = pathJoin(geospyd_version,builddate)
+local pathdir = pathJoin(miniforge_version,builddate,'envs',python_version)
 local pkgdir = pathJoin(installdir,pathdir)
 
 -- Setup Modulepath for packages built by this compiler
-local mroot = os.getenv("MODULEPATH_ROOT")
-local mdir  = pathJoin(mroot,"GEOSpyD",geospyd_version)
-prepend_path("MODULEPATH", mdir)
+-- local mroot = os.getenv("MODULEPATH_ROOT")
+-- local mdir  = pathJoin(mroot,"GEOSpyD",miniforge_version)
+-- prepend_path("MODULEPATH", mdir)
 
 unsetenv("ESMA_F2PY")
 
