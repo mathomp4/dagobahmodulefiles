a579f71d588c9f1973bbde313b8f2b4ed74e0a51
Matthew Thompson <matthew.thompson@nasa.gov>
2026-04-02 15:53:46 -0400

    Updates for nag

---
+++ Compiler/nag-7.2.43/openmpi/5.0.10.lua
@@ -0,0 +1,78 @@
+--[[
+
+NOTE 0
+
+I had to add a hack into the 5.0.10 configure script
+to work around:
+
+https://github.com/open-mpi/ompi/issues/13380
+
+----------
+
+NOTE 1
+
+You *MUST* run:
+
+unset MACOSX_DEPLOYMENT_TARGET
+
+to avoid the -dynamiclib not allowed error
+
+This is because MACOSX_DEPLOYMENT_TARGET triggers a bad codepath
+inside the configure script
+
+-----------
+
+NOTE2: Added the hwloc, libevent, and pmix line as Open MPI 5 seems to need these and
+       even if Brew can provide them (like libevent), it doesn't seem to find them
+
+This was built using:
+
+ml nag/7.2.43
+
+mkdir build-nag-7.2.43 && cd build-nag-7.2.43
+
+../configure --disable-wrapper-rpath --disable-wrapper-runpath \
+  CC=clang CXX=clang++ FC=nagfor \
+  --with-hwloc=internal --with-libevent=internal --with-pmix=internal \
+  --prefix=$HOME/installed/Compiler/nag-7.2.43/openmpi/5.0.10 |& tee configure.nag-7.2.43.log
+
+mv config.log config.nag-7.2.43.log
+make -j6 |& tee make.nag-7.2.43.log
+make install |& tee makeinstall.nag-7.2.43.log
+make check |& tee makecheck.nag-7.2.43.log
+
+--]]
+
+family("MPI")
+prereq("nag/7.2.43")
+
+local compilername = "nag-7.2.43"
+
+local version = "5.0.10"
+local compiler = pathJoin("Compiler",compilername)
+local homedir = os.getenv("HOME")
+local installdir = pathJoin(homedir,"installed")
+local pkgdir = pathJoin(installdir,compiler,"openmpi",version)
+
+-- Setup Modulepath for packages built by this MPI stack
+local mroot = os.getenv("MODULEPATH_ROOT")
+local mdir = pathJoin(mroot,"MPI/nag-7.2.43",("openmpi-"..version))
+prepend_path("MODULEPATH", mdir)
+
+setenv("OPENMPI",pkgdir)
+setenv("MPIHOME",pkgdir)
+setenv("MPI_HOME",pkgdir)
+
+-- OpenMPI has a bug on macOS that requires a short TMPDIR (if it's
+-- too long, mpirun fails)
+setenv("TMPDIR","/tmp")
+
+prepend_path("PATH",pathJoin(pkgdir,"bin"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(pkgdir,"lib"))
+prepend_path("DYLD_LIBRARY_PATH",pathJoin(pkgdir,"lib"))
+prepend_path("LIBRARY_PATH",pathJoin(pkgdir,"lib"))
+prepend_path("INCLUDE",pathJoin(pkgdir,"include"))
+prepend_path("MANPATH",pathJoin(pkgdir,"share/man"))
+
+-- setenv("OMPI_MCA_btl_tcp_if_include","lo0")
+setenv("OMPI_MCA_btl","^tcp")
