ccc2a9d613a5de5e53964cee5312765dfbed2d63
Matthew Thompson <matthew.thompson@nasa.gov>
2026-05-08 10:26:38 -0400

    Baselibs 8.30.0

--- MPI/clang-gfortran-15/openmpi-5.0.10/Baselibs/8.30.0.lua
+++ MPI/clang-gfortran-15/openmpi-5.0.10/Baselibs/8.30.0.lua
@@ -2,15 +2,15 @@
 
 This was cloned with:
 
-git clone -j 4 --recurse-submodules -b v8.24.0 https://github.com/GEOS-ESM/ESMA-Baselibs.git ESMA-Baselibs-8.24.0/src
+git clone -j 4 --recurse-submodules -b v8.30.0 https://github.com/GEOS-ESM/ESMA-Baselibs.git ESMA-Baselibs-8.30.0/src
 
 This was built using:
 
-ml clang-gfortran/15 openmpi/5.0.7
+ml clang-gfortran/15 openmpi/5.0.10
 
 NOTE: We seem to need ESMF_BOPT=g again to avoid some ESMF/MAPL issues
 
-make -j6 install ESMF_COMM=openmpi ESMF_COMPILER=gfortranclang ESMF_BOPT=g prefix=$HOME/installed/MPI/clang-gfortran-15/openmpi-5.0.7/Baselibs/8.24.0/Darwin |& tee makeinstall.clang-gfortran-15_openmpi-5.0.7.log
+make -j6 install ESMF_COMM=openmpi ESMF_COMPILER=gfortranclang ESMF_BOPT=g prefix=$HOME/installed/MPI/clang-gfortran-15/openmpi-5.0.10/Baselibs/8.30.0/Darwin |& tee makeinstall.clang-gfortran-15_openmpi-5.0.10.log
 
 NOTE: To build curl on Parcel, I had to do:
 
@@ -35,7 +35,7 @@ as udunits2 needs makeinfo.
 family("Baselibs")
 
 local compilername = "clang-gfortran-15"
-local mpiname = "openmpi-5.0.7"
+local mpiname = "openmpi-5.0.10"
 
 local version = myModuleVersion()
 local pathdir = pathJoin("MPI",compilername,mpiname)
